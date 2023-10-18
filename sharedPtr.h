b26d90edf40eb4309ecdcf075c22eaa58d9bc0fe
Maria <144431034+MariaNSU@users.noreply.github.com>
2023-10-18 15:15:49 +0700

    Changes in header

--- sharedPtr.h
+++ sharedPtr.h
@@ -6,33 +6,39 @@ class SharedPTR {
     using t_SharedPTR = SharedPTR<Type, TDeleter>;
 private:
     Type * ptr;
-    int counter; 
-public: // Constructors and destructor.
+    size_t counter; 
+public: 
+    // Constructors and destructor.
     SharedPTR() {ptr = NULL; counter = 0;}
     SharedPTR(Type *pObj) {ptr = pObj; counter = 0;}
+    // Move constructor.
     SharedPTR(t_SharedPTR &&uniquePTR) { 
         ptr = uniquePTR.ptr;
         counter = uniquePTR.counter;
         uniquePTR.ptr = NULL;
         uniquePTR.counter = 0;
 
-    } // Move constructor.
+    } 
     SharedPTR(const t_SharedPTR& obj) {
         ptr = obj.ptr;
         counter++;
     }
     ~SharedPTR() {
         if(ptr != NULL && counter == 0) {
-            // TDeleter(ptr);
+            TDeleter()(ptr);
         }
         else {
             counter--;
         }
     }
 public: // Assignment.
-    t_SharedPTR& operator=(t_SharedPTR &&sharedPTR);
+    t_SharedPTR& operator=(t_SharedPTR &&sharedPTR) {
+        ptr = sharedPTR.ptr;
+        return *this;
+    }
     t_SharedPTR& operator=(Type *pObject) {
         ptr = pObject;
+        return *this;
     }
     t_SharedPTR& operator=(const t_SharedPTR& obj) {
         ptr = obj.ptr;
@@ -43,7 +49,8 @@ public: // Observers.
     Type& operator*() const {return *ptr;}// Dereference the stored pointer.
     Type* operator->() const {return ptr;} // Return the stored pointer.
     Type* get() const {return ptr;} // Return the stored pointer.
-    TDeleter& get_deleter(); // Return a reference to the stored deleter.
+    TDeleter& get_deleter() { return TDeleter(); } // Return a reference to the stored deleter.
+    // Return false if the stored pointer is null.
     operator bool() const {
         if(ptr == NULL) {
             return false;
@@ -51,19 +58,23 @@ public: // Observers.
         else {
             return true;
         }
-    } // Return false if the stored pointer is null.
-public: // Modifiers.
-    void release() {counter--;} // Release ownership of any stored pointer.
-    void reset(Type *pObject = nullptr) {ptr = pObject;}// Replace the stored pointer.
-    void swap(t_SharedPTR &sharedPTR) {
-        T & tmp = ptr;
-        int count = counter;
+    } 
+public: // Modifiers
 
-        ptr = sharedPTR.ptr;
-        sharedPTR.ptr = tmp;
-        
-        counter = sharedPTR.counter;
-        sharedPTR.counter = count
-    } // Exchange the pointer with another object.
+    // Release ownership of any stored pointer.
+    void release() {
+        if(counter > 0) {
+            counter--;
+        }
+        else 
+            return;} 
+    // Replace the stored pointer.
+    void reset(Type *pObject = NULL) {ptr = pObject;}
+
+    // Exchange the pointer with another object.
+    void swap(t_SharedPTR &sharedPTR) {
+        std::swap(counter,sharedPTR.counter);
+        std::swap(ptr,sharedPTR.ptr);
+    } 
 };
 
