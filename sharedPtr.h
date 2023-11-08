0dc936c716f8be0de22d5d34056dbf1a223d7c55
Maria <144431034+MariaNSU@users.noreply.github.com>
2023-11-08 15:42:47 +0700

    Added makeShared
    All tests are passed succesfully

--- sharedPtr.h
+++ sharedPtr.h
@@ -1,9 +1,6 @@
 #pragma once
 #include <iostream>
 #include <stddef.h>
-
-// Ошибок много, пройдусь по каждой функции.
-
 template<typename Type, class TDeleter = std::default_delete<Type> >
 class SharedPTR {
     using t_SharedPTR = SharedPTR<Type, TDeleter>;
@@ -11,143 +8,125 @@ private:
     Type* ptr = nullptr;
     size_t* counter = nullptr;
 
-    // Если выделить память у counter, то else не нужен
     void incCounter() {
         if(ptr) {
-            *counter++;
-        }
-        else {
-            *counter = 0;
+            *counter+=1;
         }
     }
 public:
     // Constructors and destructor.
     SharedPTR() = default;
-
-// Не выделяешь память под counter через new. Поэтому у тебя не будет синхронизации счетчика ссылок между Shared Pointer'ами, которые ссылаются на один ресурс.
-// И еще один вопрос: если pObj это nullptr, надо ли делать incCounter и вообще заводить его?)
-// И используй пожалуйста во всех конструкторах member initializer list.
     SharedPTR(Type *pObj) {
-        ptr = pObj; 
-        incCounter();
+        if(pObj != nullptr) {
+            ptr = pObj;
+            counter = new size_t(0);
+            incCounter();
+        }
+        
         }
     // Move constructor.
-    SharedPTR(t_SharedPTR &&uniquePTR) {
-        ptr = uniquePTR.ptr;
-        counter = uniquePTR.counter;
+    SharedPTR(t_SharedPTR &&uniquePTR): ptr(uniquePTR.ptr), counter(uniquePTR.counter) {
         uniquePTR.ptr = nullptr;
-        uniquePTR.counter = 0;
+        uniquePTR.counter = nullptr;
     }
-    SharedPTR(const t_SharedPTR& obj) {
-        ptr = obj.ptr;
-        counter = obj.counter;
+    SharedPTR(const t_SharedPTR& obj): ptr(obj.ptr), counter(obj.counter) {
         incCounter();
     }
     ~SharedPTR() {
         release();
     }
 public: // Assignment.
-
-    // А как же освободить this от владения текущим ресурсом. Надо сделать release.
     t_SharedPTR& operator=(t_SharedPTR &&sharedPTR) {
         if(ptr == sharedPTR.ptr) {
             return *this;
         }
+        release();
         ptr = sharedPTR.ptr;
         counter = sharedPTR.counter;
         sharedPTR.ptr = nullptr;
         sharedPTR.counter = nullptr;
         return *this;
     }
-
-    //Можно сделать в одну строчку типа *this = t_SharedPTR(pObject) что ли...
     t_SharedPTR& operator=(Type *pObject) {
-        if(ptr == pObject){
-            return *this;
-        }
-        else {
-            ptr = pObject;
-            return *this;
-        }
+        *this = t_SharedPTR(pObject);
+        return *this;
         
     }
-    //То же самое, надо release сделать
     t_SharedPTR& operator=(const t_SharedPTR& obj) {
         if(ptr == obj.ptr) {
             return *this;
         }
+        release();
         ptr = obj.ptr;
         counter = obj.counter;
         incCounter();
         return *this;
     }
 public: // Observers.
-
-
     // Dereference the stored pointer.
-    // Здесь можно вернуть *get()
     Type& operator*() const {
-        if(ptr!= nullptr)
-            return *ptr;
-        else{
-            throw std::invalid_argument("ptr is NULL");
-        }}
+        return *(get());
+        }
     // Return the stored pointer.
-    //Здесь можно вернуть просто get()
     Type* operator->() const {
-        if(ptr!= nullptr)
-            return ptr;
-        else{
-            throw std::invalid_argument("ptr is NULL");
-        }} 
-    // Return the stored pointer. 
+        return get();}
+    // Return the stored pointer.
     Type* get() const {
         return ptr;
-        } 
+        }
     // Return a reference to the stored deleter.
-    TDeleter& get_deleter() {
-        return TDeleter(); 
-        } 
+    TDeleter& get_deleter() const{
+        return TDeleter();
+        }
     // Return false if the stored pointer is null.
     operator bool() const {
-        return(ptr != nullptr); 
+        return(ptr != nullptr);
         }
 public:
      // Modifiers
 
     // Release ownership of any stored pointer.
-    // последний if не нужен, просто делаешь (*counter)--
-    // А вот во втором if нужно почистить ресурсы counter тоже, ну и в конце все указатели присвоить nullptr
     void release() {
         if (counter == nullptr) {
             return;
         }
-        if(*counter == 1) {
+        if(*(counter) == 1) {
             TDeleter()(ptr);
+            delete counter;
+            ptr = nullptr;
+            counter = nullptr;
             return;
         }
-        if(counter != nullptr){
-            *counter--;
-        }
+        *(counter)-=1;
+        ptr = nullptr;
+        counter = nullptr;
+
     }
     // Replace the stored pointer.
-    // Здесь можно вообще в одну строчку через оператор присваивания нового shared pointer
     void reset(Type *pObject = nullptr) {
-        if(ptr == pObject) {
-            std::invalid_argument("Nothing to reset");
-        }
-        else {
-            pObject = ptr;
-        }
-        }
+        *this = SharedPTR(pObject); 
+    }
 
     // Exchange the pointer with another object.
     void swap(t_SharedPTR &sharedPTR) {
         std::swap(counter,sharedPTR.counter);
         std::swap(ptr,sharedPTR.ptr);
     }
-
-// Добавь функцию use_count, которая будет возвращать значение counter.
-// Добавь функциб unique() см cpp_reference
-
+    int useCount() const{
+        if(counter) {
+            return *counter;
+        }
+        else {
+            return 0;
+        }
+        
+    }
+    bool unique() const {
+        return (useCount() == 1);
+    }
+    
 };
+template< class T, class... Args >
+SharedPTR<T> makeShared( Args&&... args ){
+    return SharedPTR<T>(new T(std::forward<Args>(args)...));
+}
--- tests.cpp
+++ tests.cpp
@@ -36,15 +36,13 @@ TEST(Constructor, PointerToObj){
 }
 
 TEST(Constructor, Move){
-    int* p1 = new int[100];
-    SharedPTR<int> ptr(std::move(p1));
-    EXPECT_TRUE(ptr.get());
-    EXPECT_FALSE(p1);
-
     Example * p2 = new Example(new int [10]);
-    SharedPTR <Example> ptr2(std::move(p2));
-    EXPECT_TRUE(ptr2.get());
-    EXPECT_FALSE(p2);
+    SharedPTR<Example> ptr(p2);
+    EXPECT_TRUE(ptr);
+
+    SharedPTR <Example> ptr2(std::move(ptr));
+    EXPECT_TRUE(ptr2);
+    EXPECT_FALSE(ptr);
     
 }
 
@@ -103,19 +101,55 @@ TEST(Operators,Dereference){
     Example * p2 = new Example(new_int);
 
     SharedPTR <Example> ptr2(p2);
-    //EXPECT_EQ(*ptr2, p2); ????????
+
     EXPECT_EQ(ptr2->val, new_int);
     ptr2->multiply(0);
     EXPECT_EQ(p2->val[0], 0);
     
+    
 }
-
 TEST(Release,NoErrors){
     int * new_int = new int[10];
     Example * p2 = new Example(new_int);
     SharedPTR <Example> ptr2(p2);
     SharedPTR <Example> ptr(ptr2);
+    EXPECT_EQ(ptr2.useCount(), 2);
+    EXPECT_EQ(ptr.useCount(), 2);
     ptr2.release();
+    EXPECT_EQ(ptr2.get(), nullptr);
+    EXPECT_FALSE(ptr2.unique());
+    EXPECT_TRUE(ptr.unique());
+    ptr.release();
+    EXPECT_EQ(ptr.get(), nullptr);
+    EXPECT_EQ(ptr.useCount(), 0);
+    EXPECT_EQ(ptr2.get(),nullptr );
+    EXPECT_EQ(ptr2.useCount(), 0);
+
+}
+
+
+TEST(Reset, NoErrors) {
+    double * d = new double[100];
+    SharedPTR <double> dPtr;
+    dPtr.reset(d);
+    EXPECT_EQ(dPtr.get(), d);
+    SharedPTR <double> dPtr_(dPtr);
+}
+
+TEST(Swap, NoErrors) {
+    SharedPTR <Example> ptr1 (new Example(new int[100]));
+    SharedPTR <Example> ptr2 (ptr1);
+    SharedPTR <Example> ptr3;
+    ptr3.swap(ptr2);
+    EXPECT_EQ(ptr3.get(), ptr1.get());
     EXPECT_EQ(ptr2.get(), nullptr);
 }
 
+TEST(MakeShared, NoErrors) {
+    int * new_int = new int[10];
+    auto ptr = makeShared<Example>(new_int);
+    EXPECT_TRUE(ptr.unique());
+    EXPECT_EQ(ptr.get()->val, new_int);
+    auto ptr2 = makeShared<double>(100);
+    EXPECT_TRUE(ptr2.unique());
+}
