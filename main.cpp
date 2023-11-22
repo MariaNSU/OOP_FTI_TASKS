741ba6a325e0f9d5686481c8672ee94f240dbdcf
Mixail-laboratory <93642647+Mixail-laboratory@users.noreply.github.com>
2023-11-22 20:14:51 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,10 +17,12 @@ void handle(int N) {
 }
 
 int main() {
+    // Почему фиксированное число файлов.
     int numberOfFiles = 4;
     std::vector <std::future<void>> tasks;
     for (int i = 1; i <= numberOfFiles; i++){
         auto task = std::async (std::launch::async, handle, i);
+        //может тогда emplace_back раз уж делаешь std::move
         tasks.push_back(std::move(task));
     }
     for (int i = 1; i <= numberOfFiles; i++){
